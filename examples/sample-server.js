3df34ef5ce359e1b4ceea543581f8387fa73c2cd
codyrigney92 <crigney@sensourceinc.com>
2016-08-31 09:33:42 -0400

    Fixed path bug and added namespace to example.

--- examples/sample-server.js
+++ examples/sample-server.js
@@ -15,7 +15,7 @@ var server = new sftps3.SFTPS3Server(s3);
 server.addHostKey(fs.readFileSync(path.join(__dirname, 'keys/server_key_rsa')));
 
 //Add users' keys
-server.addPublicKey(fs.readFileSync(path.join(__dirname, 'keys/id_rsa.pub')), 'someuser');
+server.addPublicKey(fs.readFileSync(path.join(__dirname, 'keys/id_rsa.pub')), 'someuser', 'uploads');
 
 server.listen(2222, '127.0.0.1', function(port) {
   console.log('Listening on ' + port);
