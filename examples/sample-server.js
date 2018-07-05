c280e368548767bb09b31a85ea5eced0e3f4727a
Cody Rigney <cody@truly.co>
2018-07-05 00:29:31 -0400

    Update sample to use env file.

--- examples/sample-server.js
+++ examples/sample-server.js
@@ -4,12 +4,21 @@ var AWS = require('aws-sdk');
 var path = require('path');
 var fs = require('fs');
 
-//You will need your own aws.json placed at the root folder
-AWS.config.loadFromPath(path.join(__dirname, '../aws.json'));
+var defaultRegion = process.env.AWS_DEFAULT_REGION
+var s3Bucket = process.env.AWS_S3_BUCKET
+var s3Prefix = process.env.AWS_S3_PREFIX
+var sftpPort = process.env.SFTP_PORT
+var loggingEnabled = process.env.LOGGING_ENABLED
 
-var s3 = new AWS.S3({ params: { Bucket: 'sensource-testing' } });
+AWS.config.s3 = {
+  region: defaultRegion
+};
+
+var s3 = new AWS.S3({ params: { Bucket: s3Bucket } });
 
 var server = new sftps3.SFTPS3Server(s3);
+if(loggingEnabled)
+  server.enableLogging();
 
 //When using this module, make sure you generate your own key with openssl!
 server.addHostKey(fs.readFileSync(path.join(__dirname, 'keys/server_key_rsa')));
@@ -17,7 +26,7 @@ server.addHostKey(fs.readFileSync(path.join(__dirname, 'keys/server_key_rsa')));
 //Add users' keys
 server.addPublicKey(fs.readFileSync(path.join(__dirname, 'keys/id_rsa.pub')), 'someuser', 'uploads');
 
-server.listen(2222, '127.0.0.1', function(port) {
+server.listen(sftpPort, '127.0.0.1', function(port) {
   console.log('Listening on ' + port);
 });
 
