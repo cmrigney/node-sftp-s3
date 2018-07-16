0c19d9cbb82b20aadd51020419bb1e63db48c2a3
Cody Rigney <cody@truly.co>
2018-07-16 18:12:32 -0400

    Export some stuff.

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
 import SFTPS3Server from './lib/node-sftp-s3';
 import InMemoryAuthHandler from './lib/InMemoryAuthHandler';
 import { AuthHandler, AuthResult } from './lib/types';
+import { PublicKey, utils } from 'ssh2';
+const parseKey = utils.parseKey;
 
-export { SFTPS3Server, InMemoryAuthHandler, AuthHandler, AuthResult };
+export { SFTPS3Server, InMemoryAuthHandler, AuthHandler, AuthResult, PublicKey, parseKey };
  
