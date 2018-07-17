fc9b299dd0ff76d21923f9e10c7374ea103f6ba3
Cody Rigney <cody@truly.co>
2018-07-16 23:32:53 -0400

    export genPublicKey.

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import InMemoryAuthHandler from './lib/InMemoryAuthHandler';
 import { AuthHandler, AuthResult } from './lib/types';
 import { PublicKey, utils } from 'ssh2';
 const parseKey = utils.parseKey;
+const genPublicKey = utils.genPublicKey;
 
-export { SFTPS3Server, InMemoryAuthHandler, AuthHandler, AuthResult, PublicKey, parseKey };
+export { SFTPS3Server, InMemoryAuthHandler, AuthHandler, AuthResult, PublicKey, parseKey, genPublicKey };
  
